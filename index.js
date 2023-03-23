a038267859bb449624c7392a21133b3848bfb109
Lee <lee.jensen@agilix.com>
2023-03-23 07:58:47 -0600

    fix(comments)

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const app = express();
 
-// The service port. In production the application is statically hosted by the service on the same port.
+// The service port. In production the front-end code is statically hosted by the service on the same port.
 const port = process.argv.length > 2 ? process.argv[2] : 3000;
 
 // JSON body parsing using built-in middleware
 app.use(express.json());
 
-// Serve up the application's static content
+// Serve up the front-end static content hosting
 app.use(express.static('public'));
 
 // Router for service endpoints
