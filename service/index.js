1793fe742471dd48faa7a1ac234d63abd51503a1
Lee <lee.jensen@agilix.com>
2024-08-13 13:53:17 -0600

    migrate() to react

--- service/index.js
+++ service/index.js
@@ -1,5 +1,9 @@
 const express = require('express');
+const uuid = require('uuid');
 const app = express();
+
+let users = {};
+let scores = [];
 
 // The service port. In production the front-end code is statically hosted by the service on the same port.
 const port = process.argv.length > 2 ? process.argv[2] : 3000;
@@ -13,6 +17,51 @@ app.use(express.static('public'));
 // Router for service endpoints
 var apiRouter = express.Router();
 app.use(`/api`, apiRouter);
+
+// CreateAuth a new user
+apiRouter.post('/auth/create', async (req, res) => {
+  const user = users[req.body.email];
+  if (user) {
+    res.status(409).send({ msg: 'Existing user' });
+  } else {
+    const user = { email: req.body.email, password: req.body.password, token: uuid.v4() };
+    users[user.email] = user;
+
+    res.send({ token: user.token });
+  }
+});
+
+// GetAuth login an existing user
+apiRouter.post('/auth/login', async (req, res) => {
+  const user = users[req.body.email];
+  if (user) {
+    if (req.body.password === user.password) {
+      user.token = uuid.v4();
+      res.send({ token: user.token });
+      return;
+    }
+  }
+  res.status(401).send({ msg: 'Unauthorized' });
+});
+
+// DeleteAuth token if stored in cookie
+apiRouter.delete('/auth/logout', (req, res) => {
+  const user = users[req.body.email];
+  if (user) {
+    delete user.token;
+  }
+  res.status(204).end();
+});
+
+// GetUser returns information about a user
+apiRouter.get('/user/:email', async (req, res) => {
+  const user = users[req.body.email];
+  if (user) {
+    res.send({ email: user.email, authenticated: token === user.token });
+    return;
+  }
+  res.status(404).send({ msg: 'Unknown' });
+});
 
 // GetScores
 apiRouter.get('/scores', (_req, res) => {
@@ -36,7 +85,6 @@ app.listen(port, () => {
 
 // updateScores considers a new score for inclusion in the high scores.
 // The high scores are saved in memory and disappear whenever the service is restarted.
-let scores = [];
 function updateScores(newScore, scores) {
   let found = false;
   for (const [i, prevScore] of scores.entries()) {
