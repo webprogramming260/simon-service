293c72b744f6e82b6fde2206e4ab2af485668245
Donovan <21dbly@gmail.com>
2024-12-03 16:33:54 -0700

    changed source link to go to simon-service github instead of simon-react github

--- src/app.jsx
+++ src/app.jsx
@@ -74,7 +74,7 @@ function App() {
         <footer className='bg-dark text-dark text-muted'>
           <div className='container-fluid'>
             <span className='text-reset'>Author Name(s)</span>
-            <a className='text-reset' href='https://github.com/webprogramming260/simon-react'>
+            <a className='text-reset' href='https://github.com/webprogramming260/simon-service'>
               Source
             </a>
           </div>
