1c445ef971524e5f209d356f7eb3001c3a40d95d
lee <leesjensen@gmail.com>
2025-01-18 11:59:10 -0700

    upgrade() npm packages migrate(login) to service from login so it can become db

--- service/index.js
+++ service/index.js
@@ -1,9 +1,13 @@
+const cookieParser = require('cookie-parser');
+const bcrypt = require('bcrypt');
 const express = require('express');
 const uuid = require('uuid');
 const app = express();
 
+const authCookieName = 'token';
+
 // The scores and users are saved in memory and disappear whenever the service is restarted.
-let users = {};
+let users = [];
 let scores = [];
 
 // The service port. In production the front-end code is statically hosted by the service on the same port.
@@ -11,6 +15,9 @@ const port = process.argv.length > 2 ? process.argv[2] : 3000;
 
 // JSON body parsing using built-in middleware
 app.use(express.json());
+
+// Use the cookie parser middleware for tracking authentication tokens
+app.use(cookieParser());
 
 // Serve up the front-end static content hosting
 app.use(express.static('public'));
@@ -21,24 +28,24 @@ app.use(`/api`, apiRouter);
 
 // CreateAuth a new user
 apiRouter.post('/auth/create', async (req, res) => {
-  const user = users[req.body.email];
-  if (user) {
+  if (await findUser('email', req.body.email)) {
     res.status(409).send({ msg: 'Existing user' });
   } else {
-    const user = { email: req.body.email, password: req.body.password, token: uuid.v4() };
-    users[user.email] = user;
+    const user = await createUser(req.body.email, req.body.password);
 
-    res.send({ token: user.token });
+    setAuthCookie(res, user.token);
+    res.send({ email: user.email });
   }
 });
 
 // GetAuth login an existing user
 apiRouter.post('/auth/login', async (req, res) => {
-  const user = users[req.body.email];
+  const user = await findUser('email', req.body.email);
   if (user) {
-    if (req.body.password === user.password) {
+    if (await bcrypt.compare(req.body.password, user.password)) {
       user.token = uuid.v4();
-      res.send({ token: user.token });
+      setAuthCookie(res, user.token);
+      res.send({ email: user.email });
       return;
     }
   }
@@ -46,23 +53,39 @@ apiRouter.post('/auth/login', async (req, res) => {
 });
 
 // DeleteAuth logout a user
-apiRouter.delete('/auth/logout', (req, res) => {
-  const user = Object.values(users).find((u) => u.token === req.body.token);
+apiRouter.delete('/auth/logout', async (req, res) => {
+  const user = await findUser('token', req.cookies[authCookieName]);
   if (user) {
     delete user.token;
   }
+  res.clearCookie(authCookieName);
   res.status(204).end();
 });
 
+// Middleware to verify that the user is authorized to call an endpoint
+const verifyAuth = async (req, res, next) => {
+  const user = await findUser('token', req.cookies[authCookieName]);
+  if (user) {
+    next();
+  } else {
+    res.status(401).send({ msg: 'Unauthorized' });
+  }
+};
+
 // GetScores
-apiRouter.get('/scores', (_req, res) => {
+apiRouter.get('/scores', verifyAuth, (_req, res) => {
   res.send(scores);
 });
 
 // SubmitScore
-apiRouter.post('/score', (req, res) => {
-  scores = updateScores(req.body, scores);
+apiRouter.post('/score', verifyAuth, (req, res) => {
+  scores = updateScores(req.body);
   res.send(scores);
+});
+
+// Default error handler
+app.use(function (err, req, res, next) {
+  res.status(500).send({ type: err.name, message: err.message });
 });
 
 // Return the application's default page if the path is unknown
@@ -70,12 +93,8 @@ app.use((_req, res) => {
   res.sendFile('index.html', { root: 'public' });
 });
 
-app.listen(port, () => {
-  console.log(`Listening on port ${port}`);
-});
-
 // updateScores considers a new score for inclusion in the high scores.
-function updateScores(newScore, scores) {
+function updateScores(newScore) {
   let found = false;
   for (const [i, prevScore] of scores.entries()) {
     if (newScore.score > prevScore.score) {
@@ -95,3 +114,35 @@ function updateScores(newScore, scores) {
 
   return scores;
 }
+
+async function createUser(email, password) {
+  const passwordHash = await bcrypt.hash(password, 10);
+
+  const user = {
+    email: email,
+    password: passwordHash,
+    token: uuid.v4(),
+  };
+  users.push(user);
+
+  return user;
+}
+
+async function findUser(field, value) {
+  if (!value) return null;
+
+  return users.find((u) => u[field] === value);
+}
+
+// setAuthCookie in the HTTP response
+function setAuthCookie(res, authToken) {
+  res.cookie(authCookieName, authToken, {
+    secure: true,
+    httpOnly: true,
+    sameSite: 'strict',
+  });
+}
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
